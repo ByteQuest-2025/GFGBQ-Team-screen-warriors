823721cf481da1d363ac4a64428b621792a5656b
varshini63 <saiabhivarshini06@gmail.com>
2026-01-04 12:25:06 +0530

    Final commit
    Replaced the actual API key with a placeholder.

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    GEMINI_API_KEY = "AIzaSyD3-fTdrZqTJ5PXm2Miws-g7vHIcU5_J0s"
+    GEMINI_API_KEY = "YOUR_API_KEY"
     GANACHE_URL = "http://127.0.0.1:7550"
     CONTRACT_ADDRESS_GRIEVANCE = None
     CONTRACT_ADDRESS_SSI = None
